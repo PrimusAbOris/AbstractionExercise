c79571c57e54897c8f067169216958e39220e13f
PrimusAbOris <guy.ross.rit@gmail.com>
2023-09-06 11:50:05 -0400

    Cleaner animal array code

--- src/main/FarmerAlBarnyardInventoryApplication.java
+++ src/main/FarmerAlBarnyardInventoryApplication.java
@@ -10,13 +10,9 @@ public class FarmerAlBarnyardInventoryApplication {
 
 	public static void main(String[] args) {
 		// New solution
-		Cow cow = new Cow();
-		Pig pig = new Pig();
-		Sheep sheep = new Sheep();
-		Snorlax snorlax = new Snorlax();
-		Snorlax snorlax2 = new Snorlax("awake");
 		
-		Animal[] animals = {cow, pig, sheep, snorlax, snorlax2};
+		Animal[] animals = {new Cow(), new Pig(), new Sheep(),
+				new Snorlax(), new Snorlax("awake")};
 		String[] names = {"Cow", "Pig", "Sheep", "Snorlax", "Snorlax 2"};
 		
 		int i = 0;
@@ -28,40 +24,5 @@ public class FarmerAlBarnyardInventoryApplication {
 			System.out.println();
 			i++;
 		}
-		
-		
-//		Old solution		
-//		
-//		Cow cow = new Cow();
-//		System.out.println("Cow:" + "\n");
-//		cow.animalSound();
-//		cow.sleep();
-//		System.out.println();
-//		
-//		Pig pig = new Pig();
-//		System.out.println("Pig:" + "\n");
-//		pig.animalSound();
-//		pig.sleep();
-//		System.out.println();
-//		
-//		Sheep sheep = new Sheep();
-//		System.out.println("Sheep:" + "\n");
-//		sheep.animalSound();
-//		sheep.sleep();
-//		System.out.println();
-//		
-//		Snorlax snorlax = new Snorlax();
-//		System.out.println("Snorlax:" + "\n");
-//		snorlax.animalSound();
-//		snorlax.sleep();
-//		System.out.println();
-//		
-//		Snorlax snorlax2 = new Snorlax("awake");
-//		System.out.println("Snorlax 2:" + "\n");
-//		snorlax2.animalSound();
-//		snorlax2.sleep();
-//		System.out.println();
-		
 	}
-
 }
