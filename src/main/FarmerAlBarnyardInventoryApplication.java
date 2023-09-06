878afb72af16d9c4553b7ce1499389b6ae6276c6
PrimusAbOris <guy.ross.rit@gmail.com>
2023-09-06 11:14:08 -0400

    Alternative solution

--- src/main/FarmerAlBarnyardInventoryApplication.java
+++ src/main/FarmerAlBarnyardInventoryApplication.java
@@ -1,5 +1,6 @@
 package main;
 
+import animals.Animal;
 import animals.Cow;
 import animals.Pig;
 import animals.Sheep;
@@ -8,36 +9,58 @@ import animals.Snorlax;
 public class FarmerAlBarnyardInventoryApplication {
 
 	public static void main(String[] args) {
-		
+		// New solution
+		Animal[] animals = new Animal[5];
 		Cow cow = new Cow();
-		System.out.println("Cow:" + "\n");
-		cow.animalSound();
-		cow.sleep();
-		System.out.println();
-		
+		animals[0] = cow;
 		Pig pig = new Pig();
-		System.out.println("Pig:" + "\n");
-		pig.animalSound();
-		pig.sleep();
-		System.out.println();
-		
+		animals[1] = pig;
 		Sheep sheep = new Sheep();
-		System.out.println("Sheep:" + "\n");
-		sheep.animalSound();
-		sheep.sleep();
-		System.out.println();
-		
+		animals[2] = sheep;
 		Snorlax snorlax = new Snorlax();
-		System.out.println("Snorlax:" + "\n");
-		snorlax.animalSound();
-		snorlax.sleep();
-		System.out.println();
-		
+		animals[3] = snorlax;
 		Snorlax snorlax2 = new Snorlax("awake");
-		System.out.println("Snorlax 2:" + "\n");
-		snorlax2.animalSound();
-		snorlax2.sleep();
-		System.out.println();
+		animals[4] = snorlax2;
+		
+		for (Animal a : animals) {
+			a.animalSound();
+			a.sleep();
+			System.out.println();
+		}
+		
+		
+//		Old solution		
+//		
+//		Cow cow = new Cow();
+//		System.out.println("Cow:" + "\n");
+//		cow.animalSound();
+//		cow.sleep();
+//		System.out.println();
+//		
+//		Pig pig = new Pig();
+//		System.out.println("Pig:" + "\n");
+//		pig.animalSound();
+//		pig.sleep();
+//		System.out.println();
+//		
+//		Sheep sheep = new Sheep();
+//		System.out.println("Sheep:" + "\n");
+//		sheep.animalSound();
+//		sheep.sleep();
+//		System.out.println();
+//		
+//		Snorlax snorlax = new Snorlax();
+//		System.out.println("Snorlax:" + "\n");
+//		snorlax.animalSound();
+//		snorlax.sleep();
+//		System.out.println();
+//		
+//		Snorlax snorlax2 = new Snorlax("awake");
+//		System.out.println("Snorlax 2:" + "\n");
+//		snorlax2.animalSound();
+//		snorlax2.sleep();
+//		System.out.println();
+		
 	}
 
 }
